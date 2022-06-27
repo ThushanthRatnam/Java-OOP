45010e74df66ce8da607f578bcab5d56d889bad6
thushanth <thushanth@ceylonlinux.lk>
2022-06-27 10:58:17 +0530

    updated

---
+++ src/OOP/java/thushanth/OverLoading.java
@@ -0,0 +1,14 @@
+package OOP.java.thushanth;
+
+public class OverLoading {
+    public void add(int a){
+        System.out.println(a);
+    }
+    public void add(int a, int b){
+        System.out.println(a + b);
+
+    } public void add(int a, int b, int c){
+        System.out.println(a + b +c);
+    }
+
+}
